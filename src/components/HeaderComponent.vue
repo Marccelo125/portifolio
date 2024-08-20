31fe0092c20d9a15ed770199e4196d34ce56f145
Marcelo Duarte <Contato.marccelo125@gmail.com>
2024-08-20 14:29:54 -0300

    first commit

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <nav></nav>
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import HeaderComponent from '@/components/HeaderComponent.vue'
+</script>
+
+<template>
+  <header>
+    <HeaderComponent />
+  </header>
+  <main>
+    <h1 class="text-3xl font-bold">Hello im glad to see you!</h1>
+    <p>My name is <span class="font-bold text-blue-700">Marcelo Duarte</span></p>
+    <div>
+      <ol>
+        <li>
+          <a
+            href="https://github.com/marccelo125"
+            class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
+            >Github</a
+          >
+        </li>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/marcelo-duarte1/"
+            class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
+            >LinkedIn</a
+          >
+        </li>
+      </ol>
+    </div>
+  </main>
+</template>
