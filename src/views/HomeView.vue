367452c482a5296bbda2ddfa7cf17e678be2f746
Marcelo Duarte <Contato.marccelo125@gmail.com>
2024-09-03 23:27:24 -0300

    home-view-start

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,26 +6,40 @@ import HeaderComponent from '@/components/HeaderComponent.vue'
   <header>
     <HeaderComponent />
   </header>
-  <main>
-    <h1 class="text-3xl font-bold">Hello im glad to see you!</h1>
-    <p>My name is <span class="font-bold text-blue-700">Marcelo Duarte</span></p>
-    <div>
-      <ol>
-        <li>
-          <a
-            href="https://github.com/marccelo125"
-            class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
-            >Github</a
+  <main class="bg-[#1E1E24] pt-24">
+    <section>
+      <div class="flex">
+        <div class="w-4/6 bg-[#222831] m-16 p-8 rounded-md">
+          <h1
+            class="text-[#00FFAB] text-7xl"
+            style="font-family: Archivo Black; letter-spacing: 0px"
           >
-        </li>
-        <li>
-          <a
-            href="https://www.linkedin.com/in/marcelo-duarte1/"
-            class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
-            >LinkedIn</a
-          >
-        </li>
-      </ol>
-    </div>
+            Hi. I'm Marcelo. <br />
+            <span>A Developer.</span>
+          </h1>
+          <p class="mt-6 text-white text-xl">
+            I'm a a passionate about programming, web development and technology.
+          </p>
+        </div>
+        <div class="w-2/6 m-8 p-8">
+          <img src="@/assets/images/foto_perfil_linkedin.jpg" class="flex rounded-md w-5/6" />
+        </div>
+      </div>
+    </section>
+
+    <section class="grid grid-cols-3">
+      <div class="bg-blue-100"></div>
+      <div class="bg-blue-700">1</div>
+      <div class="bg-blue-900"></div>
+    </section>
   </main>
+
+  <footer>
+    <img src="@/assets/images/foto_perfil_linkedin.jpg" alt="" />
+    <div class="flex">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum id rerum voluptatum adipisci
+      iusto ducimus esse, amet eaque ipsam vero quos ea fuga sapiente sequi hic, necessitatibus
+      alias accusamus est?
+    </div>
+  </footer>
 </template>
