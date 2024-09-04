90c55d91331acd0873d1ee63f638a9ec6dc7b9ed
Marcelo Duarte <Contato.marccelo125@gmail.com>
2024-09-03 23:27:29 -0300

    header-start

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,5 +1,47 @@
 <template>
-  <div>
-    <nav></nav>
-  </div>
+  <nav class="flex align-center justify-center w-full fixed z-10 mt-4">
+    <ul class="flex gap-8 p-4 text-xl text-white font-bold">
+      <li>
+        <router-link to="/">Work /</router-link>
+      </li>
+      <li>
+        <router-link to="/about">About</router-link>
+      </li>
+      <li>
+        <router-link to="/contact">Contact</router-link>
+      </li>
+    </ul>
+  </nav>
 </template>
+
+<style scoped>
+* {
+  font-family: -apple-system, BlinkMacSystemFont, sans-serif;
+  word-spacing: 0.375rem;
+}
+
+ul {
+  border-radius: 50px;
+  background-color: rgba(35, 56, 49, 0.7);
+  backdrop-filter: blur(20px) saturate(1.7);
+}
+
+li {
+  color: rgb(0, 255, 170);
+  background-color: rgba(143, 220, 194, 0.1);
+  border: transparent 3px solid;
+  border-radius: 5rem;
+  padding: 0.2rem 1rem;
+  transition: all 0.2s;
+  cursor: pointer;
+}
+
+li:hover {
+  color: #ffffff;
+  border: transparent 3px solid;
+  background-color: rgb(0, 255, 171, 0.375);
+  border-radius: 5rem;
+  padding: 0.2rem 1.5rem;
+  cursor: pointer;
+}
+</style>
