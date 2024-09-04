d10f0400496999430a64e1e19ec5018ab9d5299b
Marcelo Duarte <Contato.marccelo125@gmail.com>
2024-09-03 23:27:07 -0300

    mdi-vue-dependencies-main-ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import mdiVue from 'mdi-vue/v3'
+import * as mdijs from '@mdi/js'
+
 const app = createApp(App)
+
+app.use(mdiVue, {
+  icons: mdijs
+})
 
 app.use(router)
 
