0d484bbbaeef197d749cf3c9df84e2efce49c464
Marcelo Duarte <Contato.marccelo125@gmail.com>
2024-09-04 04:14:16 -0300

    add-tecnologies-list

---
+++ src/components/TecnologiesList.vue
@@ -0,0 +1,83 @@
+<template>
+  <div>
+    <ul class="flex flex-wrap gap-2">
+      <li
+        class="flex font-semibold gap-1 bg-[#00ffab] text-[#124b38]"
+        style="border-color: #00ffab"
+      >
+        <mdicon name="Vuejs" />
+        VueJS
+      </li>
+      <li
+        class="flex font-semibold gap-1 bg-[#1a5c46] text-[#ffffff]"
+        style="border-color: #1a5c46"
+      >
+        <mdicon name="Nuxt" />
+        Nuxt
+      </li>
+      <li
+        class="flex font-semibold gap-1 bg-[#d1491f] text-[#ffffff]"
+        style="border-color: #d1491f"
+      >
+        <mdicon name="Laravel" />
+        Laravel
+      </li>
+      <li
+        class="flex font-semibold gap-1 bg-[#244bb6] text-[#ffffff]"
+        style="border-color: #244bb6"
+      >
+        <mdicon name="LanguageTypescript" />
+        TypeScript
+      </li>
+      <li
+        class="flex font-semibold gap-1 bg-[#4f35c2] text-[#ffffff]"
+        style="border-color: #5f40e6"
+      >
+        <mdicon name="LanguagePhp" />
+        Php
+      </li>
+      <li
+        class="flex font-semibold gap-1 bg-[#f0f324] text-[#685215]"
+        style="border-color: #f0f324"
+      >
+        <mdicon name="language-javascript" />
+        JavaScript
+      </li>
+      <li
+        class="flex font-semibold gap-1 bg-[#24a7f3] text-[#152468]"
+        style="border-color: #24a7f3"
+      >
+        <mdicon name="Tailwind" />
+        Tailwind
+      </li>
+      <li
+        class="flex font-semibold gap-1 bg-[#8b35ec] text-[#f7f7f7]"
+        style="border-color: #8b35ec"
+      >
+        <mdicon name="bootstrap" />
+        Bootstrap
+      </li>
+      <li
+        class="flex font-semibold gap-1 bg-[#aa66f7] text-[#f7f7f7]"
+        style="border-color: #aa66f7"
+      >
+        <mdicon name="DatabaseSearch" />
+        MySQL
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+li {
+  border: 1px solid;
+  border-radius: 50px;
+  padding: 0.3rem 0.675rem;
+  transition: all 0.2s;
+}
+
+li:hover {
+  transform: scale(1.05);
+  cursor: pointer;
+}
+</style>
