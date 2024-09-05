bc3ad60e3f45618816ddad04bf044ac6ed080565
Marcelo Duarte <Contato.marccelo125@gmail.com>
2024-09-04 22:37:15 -0300

    progress: aboutview first commit

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,20 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
-</template>
+<script setup lang="ts">
+import HeaderComponent from '@/components/HeaderComponent.vue'
+</script>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+<template>
+  <header>
+    <HeaderComponent />
+  </header>
+
+  <main class="pt-10">
+    <section class="flex my-8">
+      <div class="flex flex-col gap-4 text-center m-6 w-full">
+        <h1 class="text-[#00FFAB] text-6xl" style="font-family: Archivo Black; letter-spacing: 0px">
+          About me.
+        </h1>
+        <p class="text-white text-xl">123</p>
+      </div>
+    </section>
+  </main>
+</template>
