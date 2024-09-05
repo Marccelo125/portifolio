726ab6f94a1a9841602c4a33ca166261d6c5d0cb
Marcelo Duarte <Contato.marccelo125@gmail.com>
2024-09-04 22:36:44 -0300

    style: color change

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -20,17 +20,18 @@
 * {
   font-family: -apple-system, BlinkMacSystemFont, sans-serif;
   word-spacing: 0.375rem;
+  user-select: none;
 }
 
 ul {
   border-radius: 50px;
-  background-color: rgba(35, 56, 49, 0.3);
+  background-color: rgba(35, 56, 49, 0.375);
   backdrop-filter: blur(20px) saturate(1.7);
 }
 
 li {
   color: rgb(0, 255, 170);
-  background-color: rgba(143, 220, 194, 0.1);
+  background-color: rgba(143, 220, 194, 0.125);
   border: transparent 3px solid;
   border-radius: 5rem;
   padding: 0.2rem 1rem;
@@ -41,7 +42,7 @@ li {
 li:hover {
   color: #ffffff;
   border: transparent 3px solid;
-  background-color: rgb(0, 255, 171, 0.375);
+  background-color: rgb(0, 255, 171, 0.5);
   border-radius: 5rem;
   padding: 0.2rem 1.375rem;
   cursor: pointer;
