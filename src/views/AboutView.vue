698e977582bb9dd1f9642308def50b53a0607ccc
Marcelo Duarte <Contato.marccelo125@gmail.com>
2024-09-07 02:13:45 -0300

    progress: create about view

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,20 +1,25 @@
 <script setup lang="ts">
+import FooterComponent from '@/components/FooterComponent.vue'
 import HeaderComponent from '@/components/HeaderComponent.vue'
 </script>
 
 <template>
   <header>
-    <HeaderComponent />
+    <HeaderComponent active="about" />
   </header>
 
-  <main class="pt-10">
-    <section class="flex my-8">
-      <div class="flex flex-col gap-4 text-center m-6 w-full">
-        <h1 class="text-[#00FFAB] text-6xl" style="font-family: Archivo Black; letter-spacing: 0px">
-          About me.
+  <main class="mb-12 pb-0 w-full">
+    <section class="flex justify-center">
+      <div class="flex flex-col w-full lg:w-1/2 m-12 mt-28 justify-center align-center text-center">
+        <h1
+          class="text-[#00FFAB] text-4xl sm:text-6xl md:text-7xl lg:text-8xl"
+          style="font-family: Archivo Black; letter-spacing: 0px"
+        >
+          About.
         </h1>
-        <p class="text-white text-xl">123</p>
       </div>
     </section>
   </main>
+
+  <FooterComponent />
 </template>
