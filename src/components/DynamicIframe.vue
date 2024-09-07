9dda29dce2ef8297a71422fdd695f37de3923504
Marcelo Duarte <Contato.marccelo125@gmail.com>
2024-09-07 02:12:29 -0300

    progress: create dynamicIframe component

---
+++ src/components/DynamicIframe.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const props = defineProps({
+  url: {
+    type: String || undefined,
+    default: undefined
+  }
+})
+</script>
+
+<template>
+  <div class="flex flex-col justify-center aling-center">
+    <iframe :src="props.url" class="md:w-[700px] md:h-[350px] lg:w-[1200px] lg:h-[600px]" />
+  </div>
+</template>
