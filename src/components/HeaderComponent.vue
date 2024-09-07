4e2be5f4750b4444534ba9efd886ca81616515c8
Marcelo Duarte <Contato.marccelo125@gmail.com>
2024-09-07 02:12:50 -0300

    progress: edit footer component

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,17 +1,45 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+
+const props = defineProps({
+  active: {
+    type: String,
+    default: 'about'
+  }
+})
+</script>
+
 <template>
   <nav class="flex align-center justify-center w-full fixed z-10 mt-4">
     <ul class="flex gap-4 lg:gap-8 p-3 text-sm lg:text-xl text-white font-bold">
-      <li>
-        <router-link to="/" class="flex gap-1"
-          >Work <mdicon name="slash-forward-box"
-        /></router-link>
-      </li>
-      <li>
-        <router-link to="/about">About</router-link>
-      </li>
-      <li>
-        <router-link to="/contact">Contact</router-link>
-      </li>
+      <RouterLink to="/" v-if="!props.active.includes('work')" class="link-item flex gap-1">
+        Work
+        <mdicon name="slash-forward-box" />
+      </RouterLink>
+
+      <!-- ACTIVE -->
+      <RouterLink to="/" v-if="props.active.includes('work')" class="active-item flex gap-1">
+        Work
+        <mdicon name="slash-forward-box" />
+      </RouterLink>
+
+      <RouterLink to="/about" v-if="!props.active.includes('about')" class="link-item"
+        >About</RouterLink
+      >
+
+      <!-- ACTIVE -->
+      <RouterLink to="/about" v-if="props.active.includes('about')" class="active-item">
+        About
+      </RouterLink>
+
+      <RouterLink to="/contact" v-if="!props.active.includes('contact')" class="link-item"
+        >Contact</RouterLink
+      >
+
+      <!-- ACTIVE -->
+      <RouterLink to="/Contact" v-if="props.active.includes('contact')" class="active-item">
+        Contact
+      </RouterLink>
     </ul>
   </nav>
 </template>
@@ -29,7 +57,7 @@ ul {
   backdrop-filter: blur(20px) saturate(1.7);
 }
 
-li {
+.link-item {
   color: rgb(0, 255, 170);
   background-color: rgba(143, 220, 194, 0.125);
   border: transparent 3px solid;
@@ -39,7 +67,16 @@ li {
   cursor: pointer;
 }
 
-li:hover {
+.link-item:hover {
+  color: #ffffff;
+  border: transparent 3px solid;
+  background-color: rgb(0, 255, 171, 0.5);
+  border-radius: 5rem;
+  padding: 0.2rem 1.375rem;
+  cursor: pointer;
+}
+
+.active-item {
   color: #ffffff;
   border: transparent 3px solid;
   background-color: rgb(0, 255, 171, 0.5);
