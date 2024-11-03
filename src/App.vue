9681e2546160a5d979e327a13f87dfb4d7b6a4ad
Marcelo Duarte <Contato.marccelo125@gmail.com>
2024-11-03 14:05:10 -0300

    app-bg-global

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,54 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-import LanguageSwitch from '@/components/LanguageSwitch.vue'
 </script>
 
 <template>
-  <LanguageSwitch />
-  <RouterView />
+  <div>
+    <RouterView />
+  </div>
 </template>
+
+<style scoped>
+div {
+  background: linear-gradient(109deg, #111111, #161620, #1c1e29, #242736);
+  background-size: 100% 1000%;
+
+  -webkit-animation: bgDegradeAnimation 7s infinite;
+  -moz-animation: bgDegradeAnimation 7s infinite;
+  animation: bgDegradeAnimation 7s infinite;
+}
+
+@-webkit-keyframes bgDegradeAnimation {
+  0% {
+    background-position: 73% 0%;
+  }
+  50% {
+    background-position: 28% 100%;
+  }
+  100% {
+    background-position: 73% 0%;
+  }
+}
+@-moz-keyframes bgDegradeAnimation {
+  0% {
+    background-position: 73% 0%;
+  }
+  50% {
+    background-position: 28% 100%;
+  }
+  100% {
+    background-position: 73% 0%;
+  }
+}
+@keyframes bgDegradeAnimation {
+  0% {
+    background-position: 73% 0%;
+  }
+  50% {
+    background-position: 28% 100%;
+  }
+  100% {
+    background-position: 73% 0%;
+  }
+}
+</style>
