8a1ed40a544b04d5f4f1c25c55d190e4c4d75fbf
Marcelo Duarte <Contato.marccelo125@gmail.com>
2024-11-03 14:21:19 -0300

    language-switch-responsivity

--- src/components/LanguageSwitch.vue
+++ src/components/LanguageSwitch.vue
@@ -20,11 +20,11 @@ watchEffect(() => {
 </script>
 
 <template>
-  <nav class="flex align-center justify-start fixed mt-7 mx-6 gap-4">
+  <nav class="flex fixed mt-4 mx-6 gap-4 p-4 rounded-3xl">
     <button
       v-if="storedLang != 'pt'"
       @click="setLanguage('pt')"
-      class="bg-[#424242] text-[#f1f1f1] p-2 rounded-lg"
+      class="bg-[#424242] text-[#f1f1f1] py-2 px-4 rounded-3xl"
     >
       Portuguese
     </button>
@@ -32,7 +32,7 @@ watchEffect(() => {
     <button
       v-if="storedLang === 'pt'"
       @click="setLanguage('pt')"
-      class="bg-[#3e8a6a] text-[#f1f1f1] p-2 rounded-lg"
+      class="bg-[#3e8a6a] text-[#f1f1f1] py-2 px-4 rounded-3xl"
     >
       Portuguese
     </button>
@@ -40,7 +40,7 @@ watchEffect(() => {
     <button
       v-if="storedLang != 'en'"
       @click="setLanguage('en')"
-      class="bg-[#424242] text-[#f1f1f1] p-2 rounded-lg"
+      class="bg-[#424242] text-[#f1f1f1] py-2 px-4 rounded-3xl"
     >
       English
     </button>
@@ -48,7 +48,7 @@ watchEffect(() => {
     <button
       v-if="storedLang === 'en'"
       @click="setLanguage('en')"
-      class="bg-[#3e8a6a] text-[#f1f1f1] p-2 rounded-lg"
+      class="bg-[#3e8a6a] text-[#f1f1f1] py-2 px-4 rounded-3xl"
     >
       English
     </button>
@@ -58,5 +58,7 @@ watchEffect(() => {
 <style scoped>
 nav {
   z-index: 9990;
+  background-color: rgba(35, 56, 49, 0.375);
+  backdrop-filter: blur(20px) saturate(1.7);
 }
 </style>
