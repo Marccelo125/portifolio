20c606fd933406570adc899e7a37cd90396ef9c1
Marcelo Duarte <Contato.marccelo125@gmail.com>
2024-11-03 13:34:56 -0300

    add-app-language-switch

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import LanguageSwitch from '@/components/LanguageSwitch.vue'
 </script>
 
 <template>
+  <LanguageSwitch />
   <RouterView />
 </template>
