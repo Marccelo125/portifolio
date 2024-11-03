428b19c14e03adcf09f89c37d1e3041156464fac
Marcelo Duarte <Contato.marccelo125@gmail.com>
2024-11-03 13:34:46 -0300

    main-ts-config-to-vue-i18n

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import { createI18n } from 'vue-i18n'
+
+import en from './lang/en/en.json'
+import pt from './lang/pt/pt.json'
+
 import mdiVue from 'mdi-vue/v3'
 import * as mdijs from '@mdi/js'
 
@@ -13,6 +18,17 @@ app.use(mdiVue, {
   icons: mdijs
 })
 
+const i18n = createI18n({
+  legacy: false,
+  locale: localStorage.getItem('lang') || 'pt',
+  fallbackLocale: 'pt',
+  messages: {
+    en,
+    pt
+  }
+})
+
 app.use(router)
+app.use(i18n)
 
 app.mount('#app')
